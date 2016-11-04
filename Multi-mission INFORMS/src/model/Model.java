9b1ebf61801dc12fa4164fef917b86d0a26dee09
alamaranka <alamaranka@DESKTOP-S0DE36S>
2016-11-04 13:11:05 -0400

    The model is ready! I guess so :)

--- Multi-mission INFORMS/src/model/Model.java
+++ Multi-mission INFORMS/src/model/Model.java
@@ -3,21 +3,109 @@ import gurobi.*;
 
 public class Model{
 	
-	private int _theta, _m, _V, _rNode, _cNode;
+	private int _theta, _m, _V, _rNode;
+	private double[][][] _solutionOfPatroller;
 	
 	public Model(int T, int Tp, int R, int r, int c, int v){
 		_theta = T*v*r/R; _m = _theta*Tp/T;
-		_V = (r+1)*(c+1); _rNode=r+1; _cNode=c+1;
+		_V = (r+1)*(c+1); _rNode=r+1;
 	}
 	
 	public void solve(int sp, double[][][] z, double [][][] tildeX, double wn, double ws){
 		try{
 		//preliminary calculations
+		int constNum = 0; int temp= 0; int [] Kp = {_rNode};
+		for(int i=0; i<_rNode; i++){
+			Kp[i]=temp; temp+=_rNode;
+		}
+		
+		double [][] c = new double [_V][_V];
 		
 		//model
 		GRBEnv env   = new GRBEnv("Patrol");
 		GRBModel model = new GRBModel(env);    
-			
+		//variables
+		GRBVar[][][] x = new GRBVar[_V][_V][_theta];
+		for(int i=0; i<_V; i++){
+			for(int j=0; j<_V; j++){
+				for(int t=0; t<_theta; t++){
+					String s = "x"+i+j+t;
+					x[i][j][t] = model.addVar(0.0, 1.0, 0.0, GRB.BINARY,s);
+				}
+			}
+		}	
+		model.update();
+		//objective function
+		GRBLinExpr expr = new GRBLinExpr();
+			for(int i=0; i<_V; i++){
+				for(int j=0; j<_V; j++){
+					for(int t=0; t<_theta; t++){
+						expr.addTerm(wn*z[i][j][t]*c[i][j]+ws*tildeX[i][j][t]*c[i][j], x[i][j][t]);
+					}
+				}
+		}
+		model.setObjective(expr, GRB.MAXIMIZE);
+		//constraints
+		//moving only once at time t
+		for(int t=0; t<_theta; t++){
+			GRBLinExpr expr1 = new GRBLinExpr();
+			for(int i=0; i<_V; i++){
+				for(int j=0; j<_V; j++){
+					expr1.addTerm(1.0, x[i][j][t]);
+				}
+			}	
+			String s1 = "c"+constNum;
+			model.addConstr(expr1, GRB.EQUAL, 1.0, s1);
+			constNum++;
+		}
+		//starting from certain nodes
+		GRBLinExpr expr2 = new GRBLinExpr();
+		for(int k=0; k<Kp.length; k++){
+			for(int j=0; j<_V; j++){	
+				expr2.addTerm(1.0, x[k][j][sp]);
+				String s2 = "c"+constNum;
+				model.addConstr(expr2, GRB.EQUAL, 1.0, s2);
+				constNum++;
+			}
+		}
+		//ending at certain nodes
+		GRBLinExpr expr3 = new GRBLinExpr();
+		for(int i=0; i<_V; i++){
+			for(int k=0; k<Kp.length; k++){	
+				expr3.addTerm(1.0, x[i][k][sp+_m]);
+				String s3 = "c"+constNum;
+				model.addConstr(expr3, GRB.EQUAL, 1.0, s3);
+				constNum++;
+			}
+		}
+		//setting off-patrol variables to zero
+		for(int i=0; i<_V; i++){
+			for(int j=0; j<_V; j++){
+				for(int t=0; t<_theta; t++){
+					if(t<sp-1 || t>sp+_m-1){
+						GRBLinExpr expr4 = new GRBLinExpr();
+						expr4.addTerm(0.0, x[i][j][t]);
+						String s4 = "c"+constNum;
+						model.addConstr(expr4, GRB.EQUAL, 1.0, s4);
+						constNum++;
+					}
+				}
+			}
+		}
+		//solve
+		model.optimize();
+		//solution
+		_solutionOfPatroller = new double[_V][_V][_theta];
+		for(int i=0; i<_V; i++){
+			for(int j=0; j<_V; j++){
+				for(int t=0; t<_theta; t++){
+					_solutionOfPatroller[i][j][t] = x[i][j][t].get(GRB.DoubleAttr.X);
+				}
+			}
+		}	
+		//end
+		model.dispose();
+		env.dispose();	
 		}
 		catch(GRBException  e){
 			e.printStackTrace();
