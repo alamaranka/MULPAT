64fcb50fd7e8811fecc1187f41c1c04fedfdeb73
alamaranka <alamaranka@DESKTOP-S0DE36S>
2016-11-04 12:27:23 -0400

    Getting started!

--- Multi-mission INFORMS/src/model/Model.java
+++ Multi-mission INFORMS/src/model/Model.java
@@ -13,24 +13,7 @@ public class Model{
 	public void solve(int sp, double[][][] z, double [][][] tildeX, double wn, double ws){
 		try{
 		//preliminary calculations
-		double [][] c = new double[_V][_V];
-		for(int i=0; i<_V; i++){
-				if(i-1>=0){c[i][i-1]=1;}
-				if(i+1<=_cNode){c[i][i+1]=1;}
-				if(i+_cNode<=_V-1){c[i][i+_cNode]=1;}
-				if(i-_cNode>=0){c[i][i-_cNode]=1;}
-				for(int j=0; j<_V; j++){
-					if(c[i][j]!=1){
-						c[i][j] = 0;
-					}
-				}
-		}
-		for(int i=0; i<_V; i++){
-			System.out.println();
-			for(int j=0; j<_V; j++){
-				System.out.print(c[i][j]);
-			}
-		}
+		
 		//model
 		GRBEnv env   = new GRBEnv("Patrol");
 		GRBModel model = new GRBModel(env);    
