0dc6392ceb906ebd67d2fe84661f4faa5cec3935
alamaranka <alamaranka@DESKTOP-S0DE36S>
2016-11-04 10:58:39 -0400

    Second

--- Multi-mission INFORMS/src/solver/ASDASD.java
+++ Multi-mission INFORMS/src/solver/ASDASD.java
@@ -12,7 +12,7 @@ public class ASDASD {
 			if(i+_cNode<=_V-1){c[i][i+_cNode]=1;}
 			if(i-_cNode>=0){c[i][i-_cNode]=1;}
 		}
-		
+		int z = 0;
 		for(int i=0; i<_V; i++){
 			System.out.println();
 			for(int j=0; j<_V; j++){
