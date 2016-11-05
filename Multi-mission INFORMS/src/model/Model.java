e40e603fcf3ece771adaca3adf4da441f302e70b
alamaranka <alamaranka@DESKTOP-S0DE36S>
2016-11-05 15:43:17 -0400

    Adversary model is added

--- Multi-mission INFORMS/src/model/Model.java
+++ Multi-mission INFORMS/src/model/Model.java
@@ -5,33 +5,33 @@ import gurobi.*;
 
 public class Model{
 	
-	public int _theta, _m, _V, _rNode, _cNode;
+	public int _s, _m, _n, _v, _ell, _w;
 	public double[][][] _solutionOfPatroller;
 	
-	public Model(int T, int Tp, int R, int r, int c, int v){
-		_theta = T*v*r/R; _m = _theta*Tp/T;
-		_V = (r+1)*(c+1); _rNode=r+1; _cNode=c+1;
+	public Model(int T, int Tp, int Ta, int L, int W, int ell, int w, int alpha){
+		_s = T*alpha*w/W; _m = _s*Tp/T; _n = _s*Ta/T;
+		_v = w*ell; _ell = ell; _w = w;
 	}
 	
-	public void solve(int sp, double[][][] z, double wn, double ws){
+	public void solvePatroller(int sp, double[][][] zp, double wn, double ws){
 		try{
 		//preliminary calculations
-		int constNum = 0; int temp= 0; int [] Kp = {_rNode};
-		for(int i=0; i<_rNode; i++){
-			Kp[i]=temp; temp+=_rNode;
+		int constNum = 0; int temp= 0; int [] Kp = {_ell};
+		for(int i=0; i<_w; i++){
+			Kp[i]=temp; temp+=_w;
 		}
 		//random adversary action estimate
 		double [][][] tildeX = estimateTildeX();
 		//distance matrix
-		double [][] c = getDistanceMatrix();
+		double [][] c = getDistanceMatrixOfPatroller();
 		//model
 		GRBEnv env   = new GRBEnv("Patrol");
 		GRBModel model = new GRBModel(env);    
 		//variables
-		GRBVar[][][] x = new GRBVar[_V][_V][_theta];
-		for(int i=0; i<_V; i++){
-			for(int j=0; j<_V; j++){
-				for(int t=0; t<_theta; t++){
+		GRBVar[][][] x = new GRBVar[_v][_v][_s];
+		for(int i=0; i<_v; i++){
+			for(int j=0; j<_v; j++){
+				for(int t=0; t<_s; t++){
 					String s = "x"+i+j+t;
 					x[i][j][t] = model.addVar(0.0, 1.0, 0.0, GRB.BINARY,s);
 				}
@@ -40,20 +40,20 @@ public class Model{
 		model.update();
 		//objective function
 		GRBLinExpr expr = new GRBLinExpr();
-			for(int i=0; i<_V; i++){
-				for(int j=0; j<_V; j++){
-					for(int t=0; t<_theta; t++){
-						expr.addTerm(wn*z[i][j][t]*c[i][j]+ws*tildeX[i][j][t]*c[i][j], x[i][j][t]);
+			for(int i=0; i<_v; i++){
+				for(int j=0; j<_v; j++){
+					for(int t=0; t<_s; t++){
+						expr.addTerm(wn*zp[i][j][t]*c[i][j]+ws*tildeX[i][j][t]*c[i][j], x[i][j][t]);
 					}
 				}
 		}
 		model.setObjective(expr, GRB.MAXIMIZE);
 		//constraints
 		//moving only once at time t
-		for(int t=0; t<_theta; t++){
+		for(int t=0; t<_s; t++){
 			GRBLinExpr expr1 = new GRBLinExpr();
-			for(int i=0; i<_V; i++){
-				for(int j=0; j<_V; j++){
+			for(int i=0; i<_s; i++){
+				for(int j=0; j<_s; j++){
 					expr1.addTerm(1.0, x[i][j][t]);
 				}
 			}	
@@ -63,8 +63,8 @@ public class Model{
 		}
 		//starting from certain nodes
 		GRBLinExpr expr2 = new GRBLinExpr();
-		for(int k=0; k<Kp.length; k++){
-			for(int j=0; j<_V; j++){	
+		for(int k: Kp){
+			for(int j=0; j<_v; j++){	
 				expr2.addTerm(1.0, x[k][j][sp]);
 				String s2 = "c"+constNum;
 				model.addConstr(expr2, GRB.EQUAL, 1.0, s2);
@@ -73,8 +73,8 @@ public class Model{
 		}
 		//ending at certain nodes
 		GRBLinExpr expr3 = new GRBLinExpr();
-		for(int i=0; i<_V; i++){
-			for(int k=0; k<Kp.length; k++){	
+		for(int i=0; i<_v; i++){
+			for(int k: Kp){	
 				expr3.addTerm(1.0, x[i][k][sp+_m]);
 				String s3 = "c"+constNum;
 				model.addConstr(expr3, GRB.EQUAL, 1.0, s3);
@@ -82,9 +82,9 @@ public class Model{
 			}
 		}
 		//setting off-patrol variables to zero
-		for(int i=0; i<_V; i++){
-			for(int j=0; j<_V; j++){
-				for(int t=0; t<_theta; t++){
+		for(int i=0; i<_v; i++){
+			for(int j=0; j<_v; j++){
+				for(int t=0; t<_s; t++){
 					if(t<sp-1 || t>sp+_m-1){
 						GRBLinExpr expr4 = new GRBLinExpr();
 						expr4.addTerm(0.0, x[i][j][t]);
@@ -98,10 +98,10 @@ public class Model{
 		//solve
 		model.optimize();
 		//solution
-		_solutionOfPatroller = new double[_V][_V][_theta];
-		for(int i=0; i<_V; i++){
-			for(int j=0; j<_V; j++){
-				for(int t=0; t<_theta; t++){
+		_solutionOfPatroller = new double[_v][_v][_s];
+		for(int i=0; i<_v; i++){
+			for(int j=0; j<_v; j++){
+				for(int t=0; t<_s; t++){
 					_solutionOfPatroller[i][j][t] = x[i][j][t].get(GRB.DoubleAttr.X);
 				}
 			}
@@ -115,19 +115,143 @@ public class Model{
 		}
 	}
 	
-	public double[][] getDistanceMatrix(){
-		int jcol=_cNode-1; int jrow=_rNode-1;
-		double [][] c = new double [_V][_V];		
-		for(int i=0; i<_V; i++){
-			if(i%_cNode!=0){c[i][i-1]=1;}
-			if(i%_cNode!=jcol){c[i][i+1]=1;}
-			if(i<_cNode*jrow){c[i][i+_cNode]=1;}
-			if(i>jcol){c[i][i-_cNode]=1;}
+	public void solveAdversary(int sa, double[][][] za, double wr, double wc){
+		try{
+		//preliminary calculations
+		int constNum = 0; int temp= _w-1; int [] Ka = {_ell};
+		for(int i=0; i<_w; i++){
+			Ka[i]=temp; temp+=_w;
 		}
-		for(int i=0; i<_V; i++){
-			for(int j=0; j<_V; j++){
+		//random adversary action estimate
+		double [][][] tildeX = estimateTildeX();
+		//distance matrix
+		double [][] c = getDistanceMatrixOfAdversary();
+		//model
+		GRBEnv env   = new GRBEnv("Patrol");
+		GRBModel model = new GRBModel(env);    
+		//variables
+		GRBVar[][][] x = new GRBVar[_v][_v][_s];
+		for(int i=0; i<_v; i++){
+			for(int j=0; j<_v; j++){
+				for(int t=0; t<_s; t++){
+					String s = "x"+i+j+t;
+					x[i][j][t] = model.addVar(0.0, 1.0, 0.0, GRB.BINARY,s);
+				}
+			}
+		}	
+		model.update();
+		//objective function
+		GRBLinExpr expr = new GRBLinExpr();
+			for(int i=0; i<_v; i++){
+				for(int j=0; j<_v; j++){
+					for(int t=0; t<_s; t++){
+						expr.addTerm(wr*za[i][j][t]*c[i][j]+wc*tildeX[i][j][t]*c[i][j], x[i][j][t]);
+					}
+				}
+		}
+		model.setObjective(expr, GRB.MAXIMIZE);
+		//constraints
+		//moving only once at time t
+		for(int t=0; t<_s; t++){
+			GRBLinExpr expr1 = new GRBLinExpr();
+			for(int i=0; i<_s; i++){
+				for(int j=0; j<_s; j++){
+					expr1.addTerm(1.0, x[i][j][t]);
+				}
+			}	
+			String s1 = "c"+constNum;
+			model.addConstr(expr1, GRB.EQUAL, 1.0, s1);
+			constNum++;
+		}
+		//starting from certain nodes
+		GRBLinExpr expr2 = new GRBLinExpr();
+		for(int k: Ka){
+			for(int j=0; j<_v; j++){	
+				expr2.addTerm(1.0, x[k][j][sa]);
+				String s2 = "c"+constNum;
+				model.addConstr(expr2, GRB.EQUAL, 1.0, s2);
+				constNum++;
+			}
+		}
+		//ending at certain nodes
+		GRBLinExpr expr3 = new GRBLinExpr();
+		for(int i=0; i<_v; i++){
+			for(int k: Ka){	
+				expr3.addTerm(1.0, x[i][k][sa+_n]);
+				String s3 = "c"+constNum;
+				model.addConstr(expr3, GRB.EQUAL, 1.0, s3);
+				constNum++;
+			}
+		}
+		//setting off-patrol variables to zero
+		for(int i=0; i<_v; i++){
+			for(int j=0; j<_v; j++){
+				for(int t=0; t<_s; t++){
+					if(t<sa-1 || t>sa+_n-1){
+						GRBLinExpr expr4 = new GRBLinExpr();
+						expr4.addTerm(0.0, x[i][j][t]);
+						String s4 = "c"+constNum;
+						model.addConstr(expr4, GRB.EQUAL, 1.0, s4);
+						constNum++;
+					}
+				}
+			}
+		}
+		//solve
+		model.optimize();
+		//solution
+		_solutionOfPatroller = new double[_v][_v][_s];
+		for(int i=0; i<_v; i++){
+			for(int j=0; j<_v; j++){
+				for(int t=0; t<_s; t++){
+					_solutionOfPatroller[i][j][t] = x[i][j][t].get(GRB.DoubleAttr.X);
+				}
+			}
+		}	
+		//end
+		model.dispose();
+		env.dispose();	
+		}
+		catch(GRBException  e){
+			e.printStackTrace();
+		}
+	}
+	
+	public double[][] getDistanceMatrixOfPatroller(){
+		int jcol=_w-1; int jrow=_ell-1;
+		double [][] c = new double [_v][_v];		
+		for(int i=0; i<_v; i++){
+			if(i%_w!=0){c[i][i-1]=1;}
+			if(i%_w!=jcol){c[i][i+1]=1;}
+			if(i<_w*jrow){c[i][i+_w]=1;}
+			if(i>jcol){c[i][i-_w]=1;}
+		}
+		for(int i=0; i<_v; i++){
+			for(int j=0; j<_v; j++){
 				if(c[i][j]!=1){
 					c[i][j]=-Double.MAX_VALUE;
+				}
+			}
+		}
+		return c;
+	}
+	
+	public double[][] getDistanceMatrixOfAdversary(){
+		int jcol=_w-1; int jrow=_ell-1;
+		double [][] c = new double [_v][_v];		
+		for(int i=0; i<_v; i++){
+			if(i%_w!=0){c[i][i-1]=1;}
+			if(i%_w!=jcol){c[i][i+1]=1;}
+			if(i<_w*jrow){c[i][i+_w]=1;}
+			if(i>jcol){c[i][i-_w]=1;}
+		}
+		for(int i=0; i<_v; i++){
+			for(int j=0; j<_v; j++){
+				if(c[i][j]!=1){
+					c[i][j]=-Double.MAX_VALUE;
+				}
+				if(i==j){
+					c[i][j]=1;
 				}
 			}
 		}
@@ -137,19 +261,19 @@ public class Model{
 	public double[][][] estimateTildeX(){
 		Random rand = new Random(); 
 		double sum = 0;
-		double [][][] tildeX = new double [_V][_V][_theta];
-		for(int i=0; i<_V; i++){
-			for(int j=0; j<_V; j++){
-				for(int t=0; t<_theta; t++){
+		double [][][] tildeX = new double [_v][_v][_s];
+		for(int i=0; i<_v; i++){
+			for(int j=0; j<_v; j++){
+				for(int t=0; t<_s; t++){
 					double r = rand.nextDouble();
 					tildeX[i][j][t]=r;
 					sum += r;
 				}
 			}
 		}
-		for(int i=0; i<_V; i++){
-			for(int j=0; j<_V; j++){
-				for(int t=0; t<_theta; t++){
+		for(int i=0; i<_v; i++){
+			for(int j=0; j<_v; j++){
+				for(int t=0; t<_s; t++){
 					tildeX[i][j][t] /=sum;
 				}
 			}
--- Multi-mission INFORMS/src/solver/Solver.java
+++ Multi-mission INFORMS/src/solver/Solver.java
@@ -4,17 +4,21 @@ public class Solver {
 
 	public static int T = 24;
 	public static int Tp = 6;
-	public static int R = 50;
-	public static int r = 3;
-	public static int c = 3;
-	public static int v = 25;
+	public static int Ta = 6;
+	public static int W = 50;
+	public static int L = 50;
+	public static int ell = 5;
+	public static int w = 5;
+	public static int alpha = 25;
 	public static double wn = .25;
 	public static double ws = 1-wn;
+	public static double wr = .25;
+	public static double wc = 1-wr;
 	
 	public static void main(String args[]){
-		model.Model model = new model.Model(T, Tp, r, R, c, v);
-		for(int i=0; i<model._theta-model._m; i++){
-			//model.solve(i, z, wn, ws);
+		model.Model model = new model.Model(T, Tp, Ta, L, W, ell, w, alpha);
+		for(int i=0; i<model._s-model._m; i++){
+			//model.solve(i, zp, wn, ws);
 		}
 	}
 }
