f518279ee467396840d7a752716a401a23eac052
Vineet Madasseri Payyappalli <vineetma@buffalo.edu>
2016-11-05 20:15:09 -0400

    revised z_ijt

--- Multi-mission INFORMS/src/data/Data.java
+++ Multi-mission INFORMS/src/data/Data.java
@@ -4,10 +4,10 @@ public class Data {
 	int nRow = 3;
 	int nCol = 3;
 	int totalNodes = nRow*nCol;
-	int t = 10;
+	int totalTime = 10;
 	double probUpper = 1.00;
 	double probLower = 0.10; 
-	double[][][] z = new double[totalNodes][totalNodes][t];
+	double[][][] z = new double[totalNodes][totalNodes][totalTime];
 	
 	public double[][][] createRescueData(){
 		double[] probMat = new double[totalNodes];
@@ -35,17 +35,27 @@ public class Data {
 				}
 			}
 		}
-		// final z
-		for (int time=0; time<=t; time++){
+		// calculate z
+		int sum = 0;
+		for (int t=0; t<=totalTime; t++){
 			for (int i=0; i<=totalNodes; i++){
 				for (int j=0; j<=totalNodes; j++){
-					
+					z[i][j][t] = ((t<=totalTime*1/3)?1:0)*average[i][j]/3 //low probability in first 1/3 of day (12AM to 8AM)
+							+ ((t>totalTime*1/3 & t<=totalTime*2/3)? 1:0)*average[i][j]/2 //medium probability in second 1/3rd of day (8AM to 4PM)
+							+ ((t>totalTime*2/3 & t<=totalTime*5/6)? 1:0)*average[i][j] //high probability in the next 1/6th of day (4PM to 8PM)
+							+ ((t>totalTime*5/6)? 1:0)*average[i][j]/2; //medium probability in the next 1/6th of day (8PM to 12AM)
+					sum += z[i][j][t];
+				}
+			}
+		}
+		// normalize z
+		for (int t=0; t<=totalTime; t++){
+			for (int i=0; i<=totalNodes; i++){
+				for (int j=0; j<=totalNodes; j++){
+					z[i][j][t] /= sum;
 				}
 			}
 		}
 		return z;
 	}
-
-	
-	
 }
