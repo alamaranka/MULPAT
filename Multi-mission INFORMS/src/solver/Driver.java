1540710221473d99f98a3500d772023f4b0846c4
alamaranka <alamaranka@DESKTOP-S0DE36S>
2016-11-09 18:27:14 -0500

    Driver added!

---
+++ Multi-mission INFORMS/src/solver/Driver.java
@@ -0,0 +1,11 @@
+package solver;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Simulate simulation = new Simulate();
+		simulation.run();
+	}
+
+}
