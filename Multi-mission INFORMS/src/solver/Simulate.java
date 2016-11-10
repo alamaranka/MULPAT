5fe7cb4f8b453ab68e32979442f625cfb72cb7db
Vineet Madasseri Payyappalli <vineetma@buffalo.edu>
2016-11-09 21:24:29 -0500

    simulate interdiction

--- Multi-mission INFORMS/src/solver/Simulate.java
+++ Multi-mission INFORMS/src/solver/Simulate.java
@@ -1,6 +1,7 @@
 package solver;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 
 public class Simulate {
@@ -22,8 +23,8 @@ public class Simulate {
 			int p1 = patrollerPath.get(0)[2]; int p2 = patrollerPath.get(m)[2]; 
 			int a1 = adversaryPath.get(0)[2]; int a2 = adversaryPath.get(n)[2]; 
 			if(necessaryToCheckInterdiction(p1, p2, a1, a2)){
-				if(interdictionOccured()!=null){
-					updateTildeYes(interdictionOccured());
+				if(interdictionOccured(patrollerPath, adversaryPath, m, n)!=null){
+					updateTildeYes(interdictionOccured(patrollerPath, adversaryPath, m, n));
 				}else{
 					updateTildeNo();
 				}
@@ -42,18 +43,52 @@ public class Simulate {
 		//
 	}
 	
-	private int[] interdictionOccured(){
+	private int[] interdictionOccured(ArrayList<int[]> patrollerPath, ArrayList<int[]> adversaryPath, int m, int n){
+		int p1 = patrollerPath.get(0)[2]; int p2 = patrollerPath.get(m)[2]; 
+		int a1 = adversaryPath.get(0)[2]; int a2 = adversaryPath.get(n)[2]; 
 		int[] interdiction = new int[3];
-		if(2>1){
-			return interdiction;
+		if(p1>=a1){
+			for (int t=0; t<a2-p1; t++){
+				if (patrollerPath.get(t)[0]==adversaryPath.get(n-(a2-p1)+t)[0] & patrollerPath.get(t)[1]==adversaryPath.get(n-(a2-p1)+t)[1] |//for checking ij-ij interdiction
+					patrollerPath.get(t)[0]==adversaryPath.get(n-(a2-p1)+t)[1] & patrollerPath.get(t)[1]==adversaryPath.get(n-(a2-p1)+t)[0] |//for checking ij-ji interdiction	
+					patrollerPath.get(t)[1]==adversaryPath.get(n-(a2-p1)+t)[1] & adversaryPath.get(t)[0]==adversaryPath.get(n-(a2-p1)+t)[1] //for checking ij-jj interdiction
+					){
+					interdiction = patrollerPath.get(t);
+					return interdiction;
+				}
+				ArrayList<Integer> aL = getNeighbors(patrollerPath.get(t)[1]);
+				Iterator<Integer> it = aL.iterator();
+				while (it.hasNext()){
+					if (patrollerPath.get(t)[1]==adversaryPath.get(t)[1] & adversaryPath.get(t)[0] == it.next()){
+						interdiction = patrollerPath.get(t);
+						return interdiction; 
+					}
+				}
+			}
 		}
-		
-		return null;
+		else {
+			for (int t=0; t<p2-a1; t++){
+				if (patrollerPath.get(t)[0]==adversaryPath.get(m-(p2-a1)+t)[0] & patrollerPath.get(t)[1]==adversaryPath.get(m-(p2-a1)+t)[1] |//for checking ij-ij interdiction
+						patrollerPath.get(t)[0]==adversaryPath.get(m-(p2-a1)+t)[1] & patrollerPath.get(t)[1]==adversaryPath.get(m-(p2-a1)+t)[0] |//for checking ij-ji interdiction	
+						patrollerPath.get(t)[1]==adversaryPath.get(m-(p2-a1)+t)[1] & adversaryPath.get(t)[0]==adversaryPath.get(m-(p2-a1)+t)[1] //for checking ij-jj interdiction
+						){
+						interdiction = patrollerPath.get(t);
+						return interdiction;
+				}
+				ArrayList<Integer> aL = getNeighbors(patrollerPath.get(t)[1]);
+				Iterator<Integer> it = aL.iterator();
+				while (it.hasNext()){
+					if (patrollerPath.get(t)[1]==adversaryPath.get(t)[1] & adversaryPath.get(t)[0] == it.next()){
+						interdiction = patrollerPath.get(t);
+						return interdiction; 
+					}
+				}
+			}
+		}
 	}
-
-	private boolean necessaryToCheckInterdiction(int p1, int p2, int a1, int a2){
-		
-		return false;
+	
+		private boolean necessaryToCheckInterdiction(int p1, int p2, int a1, int a2){
+		return !(p1>a2 | p2>a1);	
 	}
 
 }
